75b3582e167d1a629d126736998ccdc7b90a7f0c
Connor Smith <cmsmith1217@gmail.com>
2023-09-08 14:31:54 -0600

    last push end of class

--- client/src/Movies.js
+++ client/src/Movies.js
@@ -2,29 +2,110 @@ import React, { useState, useEffect } from "react";
 
 function Movies() {
     const [movies, setMovies] = useState([]);
-    const url = "http://localhost:3001/movies";
+    const [searchName, setSearchName] = useState('');
+    const [entryInputName, setEntryInputName] = useState('');
+    const [entryInputDesc, setEntryInputDesc] = useState('');
+    const [idToDelete, setIdToDelete] = useState(0);
+
+    // const url = "http://localhost:3001/movies";
     // const url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic';
 
     useEffect(() => {
+
+        const url = searchName
+            ? `http://localhost:3001/movies?name=${searchName}`
+            : "http://localhost:3001/movies";
+
         const fetchMovies = async () => {
+            console.log(url)
             await fetch(url)
             .then((response) => response.json())
             .then((movieData) => setMovies(movieData))
-            .then((log) => console.log(movies))
         }
         console.log('movies:', movies)
         fetchMovies();
         
-    },[])
+    },[searchName, movies.length])
 
-    if(movies.length > 0) {return (
+    function reFetchMovies() {
+        const fetchMovies = async () => {
+            await fetch("http://localhost:3001/movies")
+            .then((response) => response.json())
+            .then((movieData) => setMovies(movieData))
+        }
+        fetchMovies();
+    }
+
+    //********** SEARCH FIELD FUNCTIONALITY  ***************//
+    // const handleSearch = () => {
+    //     onSearch(searchName);
+    // };
+
+    // const handleSearchSubmit = () => {
+    //     movies.filter
+    // }
+    //*****************************************************//
+
+    //************ ENTRY POST FUNCTIONALITY  **************//
+    const newEntry = async () => {
+        console.log('posting new entry')
+        await fetch("http://localhost:3001/movies", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                "name": entryInputName,
+                "haveWatched": true,
+                "toWatch": false,
+                "description": entryInputDesc
+            })
+        })
+        reFetchMovies()
+    }
+
+    const deleteEntry = async () => {
+        console.log('deleting entry')
+        await fetch(`http://localhost:3001/movies?id=${idToDelete}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+        reFetchMovies()
+    }
+    // const handleSearchSubmit = () => {
+    //     movies.filter
+    // }
+    //*****************************************************//
+
+
+    if(movies) {return (
         <>
             <h1>Movies</h1>
+            <input type='text' value={searchName} onChange={(e) => setSearchName(e.target.value)} placeholder="Search by name"/>
+            <button>Search</button>
             <ul>
                 {movies.map((movie) => (
-                <li>{movie.name}: {movie.description}</li>
+                <div>
+                    <li>{movie.name}: {movie.description}</li>
+                    <button id={`haveWatchedToggle${movie.id}`} value={movie.haveWatched}>Toggle Watched</button>
+                    <button id={`toWatchToggle${movie.id}`} value={movie.toWatch}>Toggle To Watch</button>
+                    <button id={`Delete${movie.id}`} value={movie.id} onClick={(e) => {
+                        setIdToDelete(e.target.value)
+                        deleteEntry();
+                    }}>DELETE</button>
+                </div>
                 ))}
             </ul>
+
+            <h3>Add a Movie or Show</h3>
+            <input type='text' value={entryInputName} onChange={(e) => setEntryInputName(e.target.value)} placeholder="Movie/Show Name"/>
+            <input type='text' value={entryInputDesc} onChange={(e) => setEntryInputDesc(e.target.value)} placeholder="Movie/Show Description"/>
+            <button onClick={(e) => {
+                e.preventDefault();
+                newEntry();
+            }}>Submit Entry</button>
         </>
     )}
 }
--- server/index.js
+++ server/index.js
@@ -14,14 +14,47 @@ app.get('/', (req, res) => {
 })
 
 app.get('/movies', async (req, res) => {
-    console.log('fetching movies')
-    await knex('movies')
-    .select('*')
-    .then((movies) => {
-        console.log('expressFetchMovies: ', movies)
-        res.status(200).json(movies)
-        
-    })
+    let movieNameSearch = req.query;
+    let hasQuery;
+    for (let prop in movieNameSearch) {
+        if (Object.hasOwn(movieNameSearch, prop)) {
+            hasQuery = true;
+        }
+    }
+    if(hasQuery === true) {
+        console.log('running query')
+        knex('movies')
+            .select('*').where('name', 'ilike', `%${movieNameSearch.name}%`)
+            .then((movies) => res.status(200).json(movies))
+    } else {
+        console.log('fetching movies')
+        await knex('movies')
+        .select('*')
+        .then((movies) => {
+            console.log('expressFetchMovies: ', movies)
+            res.status(200).json(movies)
+        })
+    }
+})
+
+app.post('/movies', (req, res) => {
+    const newMovieEntry = req.body;
+    console.log(newMovieEntry)
+    knex('movies')
+        .insert(newMovieEntry)
+        .then(() => {
+            res.status(201).json(`Your media entry, ${newMovieEntry.name}, has been created.`)
+            // req.session.package = req.body
+        })
+})
+
+app.delete('/movies', (req, res) => {
+    var id = parseInt(req.query.id);
+    console.log(id)
+    knex('movies')
+        .where('id', id)
+        .del()
+        .then(() => res.json(`Media entry with the id ${id} has been deleted.`))
 })
 
 app.listen(port, () => {
